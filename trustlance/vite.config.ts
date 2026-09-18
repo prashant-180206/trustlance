217f47550228a7257d71a27a1f055025f10a422c
unknown <prashantsuthar.1802@gmail.com>
2026-09-18 09:02:06 +0530

    Changed to ract

---
+++ trustlance/vite.config.ts
@@ -0,0 +1,10 @@
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import { tanstackRouter } from '@tanstack/router-plugin/vite'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    tanstackRouter(),
+    react()],
+})
